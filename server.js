50a28ab9219544d24166db8f54e3b3ecf9a15ba3
John Shimelis <johnshimelis09@gmail.com>
2026-01-18 06:07:41 +0300

    sitemap updaed

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Session configuration for Passport
 app.use(
